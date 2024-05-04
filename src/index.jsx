002c48efae6fcdfec7e8463aa87fda33643b0d36
LoBoDing <xiaoluoqn@gmail.com>
2024-05-05 00:12:46 +0800

    feat(index.jsx): myfeature - 2 commit

--- src/index.jsx
+++ src/index.jsx
@@ -17,6 +17,7 @@ function Index() {
   useInsertionEffect(() => {
     // css-in-js 插入时机，避免样式闪烁，因为此时处于 dom 未更新
     console.log("before mutaion 阶段 -- new code from myfeature");
+    console.log("before mutaion 阶段 -- new - 2 code from myfeature");
   }, []);
 
   return (
